e297586a5ffead930683778ad6b9030f43b1ba8c
hiroHamada268 <112957074+hiroHamada268@users.noreply.github.com>
2023-03-17 23:31:25 +0700

    add ui menu for btnOptions

---
+++ EatProductManager/app/src/main/java/com/example/eatproductmanager/HandleManagerActivity.java
@@ -0,0 +1,59 @@
+package com.example.eatproductmanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.eatproductmanager.Fragment.FoodsFragment;
+import com.example.eatproductmanager.Fragment.UsersFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class HandleManagerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handle_manager);
+
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FoodsFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_foods);
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_users:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UsersFragment()).commit();
+                break;
+            case R.id.nav_foods:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FoodsFragment()).commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
--- EatProductManager/app/src/main/java/com/example/eatproductmanager/ProductManagerActivity.java
+++ EatProductManager/app/src/main/java/com/example/eatproductmanager/ProductManagerActivity.java
@@ -4,17 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class ProductManagerActivity extends AppCompatActivity {
     RecyclerView.Adapter adapter;
     private RecyclerView recyclerViewCategoryList;
+
+    LinearLayout btnOptions;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_manager);
+
+        btnOptions = (LinearLayout) findViewById(R.id.btnOptions);
+        btnOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ProductManagerActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ProductManagerActivity.this, HandleManagerActivity.class ));
+            }
+        });
+
 
         recyclerViewCategory();
 
@@ -24,7 +40,5 @@ public class ProductManagerActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerViewCategoryList = findViewById(R.id.rvCategoriesHome);
         recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
-
-
     }
 }
